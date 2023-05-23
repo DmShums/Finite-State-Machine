0fc678263ed8f99a73f17abdbf7cd5705ea36307
Dmytro Shumsky <schooldima1@gmail.com>
2023-05-23 16:21:51 +0300

    main and random event addition

--- day_model.py
+++ day_model.py
@@ -1,12 +1,25 @@
+"""Finite-State-Machine"""
+
 import random
 
 class FSM:
+    """
+    Model your day in life
+    Every hour (you can take smaller time intervals if you wish)
+    you can be in one of the states 
+    """
     def __init__(self):
+        # main events
         self.sleep = self._sleep()
         self.eat = self._eat()
         self.study = self._study()
         self.cry = self._cry_bcs_of_studying()
+        self.dog = self._walk_a_dog()
+
+        # random events
         self.alarm = self._air_alarm()
+        self.music = self._music()
+        self.twitter = self._read_twitter()
 
         # setting current state of the system
         self.current_state = self.sleep
@@ -20,20 +33,26 @@ class FSM:
         next(self.eat)
         next(self.study)
         next(self.cry)
+        next(self.dog)
         next(self.alarm)
+        next(self.music)
+        next(self.twitter)
 
     def does_match(self):
-        """The function at any point in time returns if till the current input
+        """
+        The function at any point in time returns if till the current input
         the string matches the given regular expression.
 
         It does so by comparing the current state with the end state `study`.
-        It also checks for `stopped` flag which sees that due to bad input the iteration of FSM had to be stopped.
+        It also checks for `stopped` flag which sees that due to bad input the
+        iteration of FSM had to be stopped.
         """
         if self.stopped:
             return False
         return self.current_state == self.study
 
     def cycle(self):
+        """Day cycle"""
         for i in range(24):
             self.current_state.send(i)
 
@@ -56,20 +75,26 @@ class FSM:
             time = yield
             print(f'\nHour {time}')
             print('State: Eat')
-            if time in range(8, 10):
-                self.current_state = self.eat
+            if random.random() > 0.3:
+                if time in range(8, 10):
+                    self.current_state = self.eat
+                else:
+                    self.current_state = random.choice([self.study, self.sleep])
             else:
-                self.current_state = random.choice([self.study, self.sleep])
+                self.current_state = self.twitter
 
     def _study(self):
         while True:
             time = yield
             print(f'\nHour {time}')
             print('State: Study')
-            if time in range(10, 14) or time in range(16, 21) or time in range(22, 24):
-                self.current_state = self.study
+            if random.random() > 0.3:
+                if time in range(10, 14) or time in range(19, 21) or time in range(22, 24):
+                    self.current_state = self.study
+                else:
+                    self.current_state = random.choice([self.eat, self.dog])
             else:
-                self.current_state = random.choice([self.eat, self.sleep])
+                self.current_state = self.music
 
     def _cry_bcs_of_studying(self):
         while True:
@@ -81,6 +106,17 @@ class FSM:
             else:
                 self.current_state = random.choice([self.eat, self.sleep])
 
+    def _walk_a_dog(self):
+        while True:
+            time = yield
+            print(f'\nHour {time}')
+            print('State: Walk a dog')
+            if time in range(18, 19):
+                self.current_state = self.dog
+            else:
+                self.current_state = self.study
+
+
     def _air_alarm(self):
         while True:
             time = yield
@@ -91,6 +127,28 @@ class FSM:
                 self.current_state = self.eat
             else:
                 self.current_state = self.sleep
+
+    def _music(self):
+        while True:
+            time = yield
+            print(f'\nHour {time}')
+            print("State: Listen to music")
+            rand = random.random()
+            if rand < 0.9:
+                self.current_state = self.study
+            else:
+                self.current_state = self.music
+
+    def _read_twitter(self):
+        while True:
+            time = yield
+            print(f'\nHour {time}')
+            print("State: Read twitter")
+            rand = random.random()
+            if rand < 0.9:
+                self.current_state = self.eat
+            else:
+                self.current_state = self.twitter
 
 if __name__ == '__main__':
     fsm = FSM()
